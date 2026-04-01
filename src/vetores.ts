5a0050b69c84063e85f325257ebce0be50b4b237
vitao-campos <vaffonsocampos@gmail.com>
2026-03-31 21:47:09 -0300

    modifica o vetores

--- src/vetores.ts
+++ src/vetores.ts
@@ -1,5 +1,7 @@
 const numeros: number [] = [10, 20, 30, 40, 50]
 const nomes = ["victor", "affonso", "campos"];
 const geral = ["victor super choque", 32, 94, "corinthians"]
+const nomedenovo = "corinthians paulista"
 console.log(numeros[3])
-console.log(geral[2])
+console.log(geral[0])
+console.log (nomedenovo [2])
